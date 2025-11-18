7587d5dc405b59763c8a64862d45c5b6a6d2dd46
rutujamahek <rutujamahek@gmail.com>
2025-11-18 13:55:47 +0000

    update acc to create react app

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,5 @@
-// If VITE_API_BASE_URL is set (local), use it
+// If REACT_APP_API_BASE_URL is set (local), use it
 // If not (production), use "" so /api works automatically
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "";
 
 export default API_BASE_URL;
-
